2ce31a47cd1a694929b414ff195611dc808c12fb
Crowdbotics <team@crowdbotics.com>
2021-08-31 00:22:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "test_yolo_30216Identifier",
+  url: "https://test_yolo_30216Identifier.botics.co",
+  api: "https://test_yolo_30216Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
